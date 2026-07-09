686f0043b084bd2331461d6a9b5730a8ec22cda5
03-03-2001 <rohitvkamble0301@gmail.com>
2026-07-09 08:27:45 +0530

    Add backend package files and update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,13 +4,22 @@ const cors = require('cors');
 
 const todoRoutes = require('./routes/todoRoutes');
 const errorHandler = require('./middlewares/error.middleware');
-
+const connectDB = require('./config/db');
 const app = express();
+
+connectDB();
 
 
 app.use(express.json());
 
 app.use(cors());
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Todo API is running 🚀"
+  });
+});
 
 app.use('/todos', todoRoutes);
 
